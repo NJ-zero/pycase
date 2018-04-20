da3cb33abca96399b89917e0758a7b28de03c24d
NJ-Zero <821854810@qq.com>
2018-04-20 16:42:06 +0800

    兼容中文

--- str_to_json/trans.py
+++ str_to_json/trans.py
@@ -2,6 +2,10 @@
 # author='Shichao-Dong'
 # create time 2018/4/17
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 import json
 
 def read(path):
@@ -17,14 +21,18 @@ def writejson(txt,newfile):
     lines = read(txt)
     newjson = {}
     for line in lines:
-        newjson[line.split()[0]] = line.split()[1]
-    jsonobj = json.dumps(newjson)
-    try:
-        f =  open(newfile,'w')
-        f.write(jsonobj)
-        f.close()
-        print('生成文件成功')
-    except:
-        print('转化失败')
+        print(line.split()[1].decode('utf-8'))
+        value = line.split()[1].decode('utf-8')
+        newjson[line.split()[0]] = value
+
+    jsonobj = json.dumps(newjson).decode('unicode-escape')
+    print(jsonobj)
+
+    f =  open(newfile,'w')
+    f.write(jsonobj)
+    f.close()
+    print('生成文件成功')
+
+
 
 writejson('str.txt','json.txt')
